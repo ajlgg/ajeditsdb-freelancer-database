5a7e578aba025014cc7660020ca23ab21e42311a
ajlgg <luthraa223@gmail.com>
2026-05-09 23:33:49 -0400

    select queries

---
+++ schema/select_queries.sql
@@ -0,0 +1,26 @@
+SELECT * FROM clients
+LIMIT 10;
+
+SELECT * FROM freelancer
+LIMIT 10;
+
+SELECT * FROM services
+LIMIT 10;
+
+SELECT * FROM service_packages
+LIMIT 10;
+
+SELECT * FROM orders
+LIMIT 10;
+
+SELECT * FROM order_items
+LIMIT 10;
+
+SELECT * FROM payments
+LIMIT 10;
+
+SELECT * FROM reviews
+LIMIT 10;
+
+SELECT * FROM messages
+LIMIT 10;
